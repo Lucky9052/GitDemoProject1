81a5160ce98f27218c965d2b02e87c2fd1886ba6
Lucky9052 <srilakshmiyadlapalli1@gmail.com>
2023-10-29 22:23:30 +0530

    first commit...

---
+++ src/GitDEMO1.java
@@ -0,0 +1,11 @@
+
+public class GitDEMO1 {
+public static void main(String args[]) {
+	String hungry ="yes";
+	if(hungry=="yes")
+		System.out.println("eat somthing..");
+	else {
+		System.out.println("do work..");
+	}
+}
+}
